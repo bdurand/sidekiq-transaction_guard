27049f30666d00b767fb160835fe86f8f5aef505
Winston Durand <me@winstondurand.com>
2018-08-22 08:59:14 -0700

    grr

--- lib/sidekiq_transaction_guard/version.rb
+++ lib/sidekiq_transaction_guard/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module SidekiqTransactionGuard
-  VERSION = File.read(File.expand_path("../../VERSION", __FILE__), "r").chomp.freeze
+  VERSION = File.read(File.expand_path("../../../VERSION", __FILE__)).chomp.freeze
 end
